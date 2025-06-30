c6a23d099b381aa87063cd2e5fd8ed21147803f8
ibrahim kurut <ibrahim.kurut06@gmail.com>
2025-06-30 21:26:51 +0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold mb-8">Welcome to My Next.js App</h1>
+    </main>
+  );
+}
