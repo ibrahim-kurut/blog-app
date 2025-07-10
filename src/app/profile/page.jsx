94785f7f0805217230f058b0ca43ac170c80a213
ibrahim kurut <ibrahim.kurut06@gmail.com>
2025-07-10 17:05:30 +0300

    create update model

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,16 @@
+"use client";
+import UpdateProfileModel from "@/components/profile/UpdateProfileModel";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const ProfilePage = () => {
+  const [showUpdateModel, setShowUpdateModel] = useState(false);
+
+  // handle update profile
+  const handleUpdateProfile = () => {
+    setShowUpdateModel(true);
+  };
   return (
     <div className="">
       <div className="container mx-auto w-[90%] h-[70%] bg-gray-400 mt-10 rounded-lg shadow-lg p-6">
@@ -11,6 +20,7 @@ const ProfilePage = () => {
             alt="Profile Picture"
             width={150}
             height={150}
+            priority
             className="rounded-full mx-auto mb-4"
           />
           <div>
@@ -20,7 +30,10 @@ const ProfilePage = () => {
           {/* delete update profile */}
           <div className="flex justify-between items-center mt-4">
             <div className="flex gap-2">
-              <button className="bg-green-400 py-1 px-2 rounded hob=ver:bg-green-500 transition-colors hover:text-white cursor-pointer capitalize">
+              <button
+                onClick={handleUpdateProfile}
+                className="bg-green-400 py-1 px-2 rounded hob=ver:bg-green-500 transition-colors hover:text-white cursor-pointer capitalize"
+              >
                 update profile
               </button>
               <button className="bg-red-400 py-1 px-2 rounded hob=ver:bg-green-500 transition-colors hover:text-white cursor-pointer capitalize">
@@ -39,6 +52,11 @@ const ProfilePage = () => {
           </div>
         </div>
       </div>
+
+      {/* update profile model */}
+      {showUpdateModel && (
+        <UpdateProfileModel setShowUpdateModel={setShowUpdateModel} />
+      )}
     </div>
   );
 };
---
+++ src/components/profile/UpdateProfileModel.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useState } from "react";
+
+const UpdateProfileModel = ({ setShowUpdateModel }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState("");
+
+  // handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ name, email, image });
+    setName("");
+    setEmail("");
+    setImage("");
+    setTimeout(() => {
+      setShowUpdateModel(false);
+    }, 1500);
+  };
+
+  return (
+    <div>
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="bg-white p-6 rounded-lg shadow-lg w-[90%] md:w-[50%]">
+          <h2 className="text-xl font-bold mb-4">Update Profile</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            {/* name  */}
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded"
+                placeholder="Enter your name"
+              />
+            </div>
+            {/* email */}
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded"
+                placeholder="Enter your email"
+              />
+            </div>
+            {/* image */}
+            <div className="mb-4">
+              <label className="block text-sm font-medium mb-2">image</label>
+              <input
+                type="file"
+                accept="image/*"
+                onChange={(e) => setImage(e.target.files[0])}
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition-colors">
+                Update
+              </button>
+              <div
+                onClick={() => setShowUpdateModel(false)}
+                className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600 transition-colors cursor-pointer"
+              >
+                close
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpdateProfileModel;
